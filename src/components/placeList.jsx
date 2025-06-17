ee64269f0f7b8cd8f675ede19f77272c50a92a3f
tkdgheee <sangho135790@gmail.com>
2025-06-17 20:05:59 +0900

    로딩 상태, 위치에 따른 맛집 정렬 구현

--- src/components/placeList.jsx
+++ src/components/placeList.jsx
@@ -1,23 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import usePlaceStore from "../store/placeStore";
+import { sortPlacesByDistance } from "../utils/loc";
 
 const PlaceList = () => {
-  const IMAGE_BASE_URL = "http://localhost:3000/";
-
-  const placesData = usePlaceStore(state => state.places);
-  const places = placesData?.places || [];
-
-  const fetchPlaces = usePlaceStore(state => state.fetchPlaces);
+    const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
+    const placesData = usePlaceStore(state => state.places);
+    const places = placesData?.places || [];
+    const [sortedPlaces, setSortedPlaces] = useState([]);
+    const fetchPlaces = usePlaceStore(state => state.fetchPlaces);
+    const isLoading = usePlaceStore(state => state.isLoading);
+    const error = usePlaceStore(state => state.error);
 
   useEffect(() => {
     fetchPlaces();
   }, [fetchPlaces]);
 
+  useEffect(()=> {
+    if (places.length === 0) {
+      console.error("맛집 목록이 비어 있습니다.");
+      return;
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      const sorted = sortPlacesByDistance(places, latitude, longitude);
+      setSortedPlaces(sorted);
+    },
+    (error) => {
+      console.error("위치 정보를 가져오는 데 실패했습니다:", error);
+      error("위치 정보를 가져오는 데 실패했습니다. 맛집 목록을 원래 순서대로 표시합니다.");
+      setSortedPlaces(places); // 위치 정보가 없을 경우 원래 순서대로 표시
+    }
+  )}, [places]);
+
+  if(isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen text-5xl text-gray-500">
+            맛집 목록을 불러오는 중입니다...
+      </div>
+    );
+  }
+
+  if(error) {
+    return (
+      <div className="flex justify-center items-center h-screen text-5xl text-red-500">
+        맛집 목록을 불러오는 데 실패했습니다. 404 Not Found
+      </div>
+    );
+  }
+
+
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
       <h2 className="text-3xl font-bold mb-8 text-center">📍 전국 맛집 리스트</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {places.map(place => (
+        {sortedPlaces.map(place => (
           <div
             key={place.id}
             className="bg-white rounded-2xl shadow-md overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl"
--- src/store/placeStore.jsx
+++ src/store/placeStore.jsx
@@ -4,20 +4,33 @@ import { getAllPlaces, getUsersPlaces } from "../api/storeApi";
 const usePlaceStore = create((set) => ({
     places: [],
     usersPlace: [],
+    isLoading: false,
+    error: null,
+
     fetchPlaces: async () => {
+        set({isLoading: true, error: null});
         try {
             const data = await getAllPlaces();
             set({ places: data});
+            set({isLoading: false});
         }catch (error) {
             console.error('전체 맛집 불러오기 실패:', error)
+            set({ error: '맛집 목록을 불러오는 데 실패했습니다. 404 Not Found' });
+        }finally {set({isLoading: false});
         }
+        
     },
     fetchUsersPlace: async () => {
+        set({isLoading: true, error: null});
         try{
             const data = await getUsersPlaces();
             set ({usersPlace : data});
+            set({isLoading: false});
         }catch (error) {
             console.error('찜한 맛집 불러오기 실패', error)
+            set({ error: '찜한 맛집 목록을 불러오는 데 실패했습니다. 404 Not Found' });
+        }finally {
+            set({isLoading: false});
         }
     },
 }
